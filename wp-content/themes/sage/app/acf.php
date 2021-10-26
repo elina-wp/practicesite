e2c84bf1e905e64c32c8d1743c01b26e43e2b68a
elina-wp <alina@outside.tech>
2021-10-26 19:20:42 +0545

    add all files

---
+++ wp-content/themes/sage/app/acf.php
@@ -0,0 +1,39 @@
+<?php
+
+if( class_exists('ACF') ) :
+    function landmark_allowed_block_types( $allowed_blocks, $post ) {
+        $allowed_blocks = array(
+            'core/image',
+            'core/paragraph',
+            'core/heading',
+            'core/list',
+            'core/freeform',
+            'core/html',
+            'core/quote',
+            'core/shortcode',
+            'acf/leadspace-home',
+            
+        );
+
+
+        return $allowed_blocks;
+    }
+    add_filter('allowed_block_types_all', 'landmark_allowed_block_types', 10, 2);
+
+    
+
+    // ACF Theme Option
+    if (function_exists('acf_add_options_page')) {
+        acf_add_options_page(array(
+            'page_title' => 'Theme General Settings',
+            'menu_title' => 'Theme Settings',
+            'menu_slug' => 'theme-general-settings',
+            'capability' => 'edit_posts',
+            'redirect' => true
+        ));
+
+        
+    }
+
+    
+endif;
